0eedb9c83aaa3f13f936c88b237c11058258e3e9
tranvoquanghuy <tvqhuy246@users.noreply.github.com>
2025-10-28 15:23:40 +0700

    listing service

---
+++ backend/services/listing-service/routes/routeslisting.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const listingController = require("../controllers/controllerslisting.controller");
+router.get("/", listingController.getAllListings);
+
+// 🟢 Lấy tin đăng theo ID
+router.get("/:id", listingController.getListingById);
+
+// Route tạo tin đăng
+router.post("/", listingController.createListing);
+
+// Route sửa tin đănga
+router.put("/:id", listingController.updateListing);
+
+module.exports = router;
